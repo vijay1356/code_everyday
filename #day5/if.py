b85edb43948cc8ed707b4555d03ff19a7cddda6f
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2023-12-09 21:58:44 +0530

    python code

---
+++ #day5/if.py
@@ -0,0 +1,8 @@
+#Check if the given number is positive
+a=int(input("Enter the value : "))
+if a>0:
+    print("Entered value is a positive number ")
+elif a>=0:
+    print("Entered value is a positive number or 0")
+else :
+    print("Entered number is a negative number")
