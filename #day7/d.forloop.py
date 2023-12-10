1d9b3d6bf267f3540ee59d19eef128718758fd91
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2023-12-10 13:18:43 +0530

    divisible by 3

--- #day7/d.forloop.py
+++ #day7/d.forloop.py
@@ -1,6 +1,12 @@
 for i in range(20,30,2):
     print(i)
 
+print("")
 fruits=['apple','orange','banana']
 for x in fruits:
     print (x)
+
+print("")
+for i in range(55,75):
+    if i%3==0:
+        print(i)
